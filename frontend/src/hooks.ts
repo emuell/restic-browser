8c8f862c3432f083bd7d786ae2c6ca3e99ea3dfc
Eduard Müller <mail@emuell.net>
2022-06-28 08:58:50 +0200

    workaround for vaadin & vite

---
+++ frontend/src/hooks.ts
@@ -0,0 +1,18 @@
+// workaround for vaading & vite
+// see https://github.com/vaadin/vaadin-lumo-styles/issues/105
+const oldDefine = customElements.define;
+customElements.define = function(name: string, constructor: CustomElementConstructor, options?: ElementDefinitionOptions) {
+  try {
+    return oldDefine.call(customElements, name, constructor, options);
+  } catch (error) {
+      if (
+        error instanceof DOMException &&
+        error.message.includes('has already been used with this registry')
+      ) {
+        return false;
+      }
+      throw error;
+  }
+}
+
+export { }
