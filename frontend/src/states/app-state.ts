a5f24d8a0bfa001e36004622fc96ac4f712c76e2
Eduard Müller <mail@emuell.net>
2022-06-29 23:56:50 +0200

    add mobx action setters to appState

--- frontend/src/states/app-state.ts
+++ frontend/src/states/app-state.ts
@@ -23,6 +23,33 @@ export class AppState {
   @mobx.observable
   snapShots: lib.Snapshot[] = [];
 
+  @mobx.action
+  setNewRepo(path: string, error: string = "") {
+    this.repoPath = path;
+    this.repoError = error;
+    this.snapShots = [];
+    this.selectedSnapshotID = "";
+  }
+  
+  @mobx.action
+  setNewSnapshots(snapShots: lib.Snapshot[], error: string = "") {
+    this.repoError = error;
+    this.snapShots = snapShots; 
+    if (this.snapShots.findIndex((s) => s.short_id === this.selectedSnapshotID) === -1) {
+      this.selectedSnapshotID = "";
+    }
+  }
+
+  @mobx.action
+  setNewSnapshotId(id: string) {
+    if (id && this.snapShots.findIndex((s) => s.id === id) !== -1) {
+      this.selectedSnapshotID = id;
+    } 
+    else {
+      this.selectedSnapshotID = "";
+    }
+  }
+
   constructor() {
     mobx.makeObservable(this);
   }
