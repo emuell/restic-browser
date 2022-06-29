74f90a51e6473780f0d769fc943227796a83cebf
Eduard Müller <mail@emuell.net>
2022-06-29 14:44:43 +0200

    removed unused go functions

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,27 +1,5 @@
 export namespace lib {
 	
-	export class Snapshot {
-	    id: string;
-	    short_id: string;
-	    time: string;
-	    paths: string[];
-	    hostname: string;
-	    username: string;
-	
-	    static createFrom(source: any = {}) {
-	        return new Snapshot(source);
-	    }
-	
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.id = source["id"];
-	        this.short_id = source["short_id"];
-	        this.time = source["time"];
-	        this.paths = source["paths"];
-	        this.hostname = source["hostname"];
-	        this.username = source["username"];
-	    }
-	}
 	export class File {
 	    name: string;
 	    type: string;
@@ -30,14 +8,9 @@ export namespace lib {
 	    gid?: number;
 	    size?: number;
 	    mode?: number;
-	    // Go type: time.Time
-	    mtime?: any;
-	    // Go type: time.Time
-	    atime?: any;
-	    // Go type: time.Time
-	    ctime?: any;
-	    struct_type?: string;
-	    children?: {[key: string]: File};
+	    mtime?: string;
+	    atime?: string;
+	    ctime?: string;
 	
 	    static createFrom(source: any = {}) {
 	        return new File(source);
@@ -52,30 +25,34 @@ export namespace lib {
 	        this.gid = source["gid"];
 	        this.size = source["size"];
 	        this.mode = source["mode"];
-	        this.mtime = this.convertValues(source["mtime"], null);
-	        this.atime = this.convertValues(source["atime"], null);
-	        this.ctime = this.convertValues(source["ctime"], null);
-	        this.struct_type = source["struct_type"];
-	        this.children = source["children"];
+	        this.mtime = source["mtime"];
+	        this.atime = source["atime"];
+	        this.ctime = source["ctime"];
+	    }
+	}
+	export class Snapshot {
+	    id: string;
+	    short_id: string;
+	    time: string;
+	    paths: string[];
+	    tags: string[];
+	    hostname: string;
+	    username: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new Snapshot(source);
 	    }
 	
-		convertValues(a: any, classs: any, asMap: boolean = false): any {
-		    if (!a) {
-		        return a;
-		    }
-		    if (a.slice) {
-		        return (a as any[]).map(elem => this.convertValues(elem, classs));
-		    } else if ("object" === typeof a) {
-		        if (asMap) {
-		            for (const key of Object.keys(a)) {
-		                a[key] = new classs(a[key]);
-		            }
-		            return a;
-		        }
-		        return new classs(a);
-		    }
-		    return a;
-		}
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.short_id = source["short_id"];
+	        this.time = source["time"];
+	        this.paths = source["paths"];
+	        this.tags = source["tags"];
+	        this.hostname = source["hostname"];
+	        this.username = source["username"];
+	    }
 	}
 
 }
--- lib/snapshot.go
+++ lib/snapshot.go
@@ -1,16 +1,12 @@
 package lib
 
-// Snapshot hols snapshot info
+// holds snapshot info
 type Snapshot struct {
 	ID       string   `json:"id"`
 	ShortID  string   `json:"short_id"`
 	Time     string   `json:"time"`
 	Paths    []string `json:"paths"`
+	Tags     []string `json:"tags"`
 	Hostname string   `json:"hostname"`
 	Username string   `json:"username"`
-}
-
-func (s *Snapshot) GetFilesForPath(path string) ([]*File, error) {
-	// TODO!
-	return []*File{NewDir("test", nil)}, nil
 }
