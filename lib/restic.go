5704502457bbb8564b25d66ee401edcd9522b0c1
Eduard Müller <mail@emuell.net>
2022-06-30 18:00:50 +0200

    hide restic cmd window in windows prod builds

--- lib/restic.go
+++ lib/restic.go
@@ -1,22 +1,55 @@
 package lib
 
 import (
+	"bytes"
+	"fmt"
+	"os/exec"
 	"path/filepath"
 	"strings"
-	e "github.com/leaanthony/sail/errors"
-	p "github.com/leaanthony/sail/program"
+	"syscall"
+
+	"github.com/leaanthony/sail/program"
 )
 
 // Restic holds information about the Restic Binary
 type Restic struct {
 	VersionString string
-	Version string
+	Version       string
 
-	restic *p.Program
+	restic *program.Program
 }
 
 func (r *Restic) Run(command []string) (stdout, stderr string, code int, err error) {
-	return r.restic.Run(command...)
+
+	// NB: do not use r.restic.Run(command...) here. SysProcAttr is necessary for Windows
+	// to hide the cmd window:
+	// https://newbedev.com/how-to-hide-command-prompt-window-when-using-exec-in-golang
+	cmd := exec.Command(r.restic.Path, command...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+	var stdo, stde bytes.Buffer
+	cmd.Stdout = &stdo
+	cmd.Stderr = &stde
+	err = cmd.Run()
+	stdout = string(stdo.Bytes())
+	stderr = string(stde.Bytes())
+	// https://stackoverflow.com/questions/10385551/get-exit-code-go
+	if err != nil {
+		// try to get the exit code
+		if exitError, ok := err.(*exec.ExitError); ok {
+			ws := exitError.Sys().(syscall.WaitStatus)
+			code = ws.ExitStatus()
+		} else {
+			code = 1
+			if stderr == "" {
+				stderr = err.Error()
+			}
+		}
+	} else {
+		// success, exitCode should be 0 if go is ok
+		ws := cmd.ProcessState.Sys().(syscall.WaitStatus)
+		code = ws.ExitStatus()
+	}
+	return stdout, stderr, code, err
 }
 
 // NewRepo creates a new repo instance
@@ -29,12 +62,12 @@ func (r *Restic) NewRepo(path string, password string) (*Repository, error) {
 }
 
 // NewRestic creates a new Restic struct
-func NewRestic(restic *p.Program) (*Restic, error) {
-	
+func NewRestic(restic *program.Program) (*Restic, error) {
+
 	// Get the version
 	versionstring, _, code, _ := restic.Run("version")
 	if code != 0 {
-		return nil, e.New(badVersion)
+		return nil, fmt.Errorf("failed to fetch restic version from %s", restic.Path)
 	}
 
 	// Find restic version
@@ -46,11 +79,9 @@ func NewRestic(restic *p.Program) (*Restic, error) {
 
 	result := &Restic{
 		VersionString: versionstring,
-		Version: version,
-		restic: restic,
+		Version:       version,
+		restic:        restic,
 	}
 
 	return result, nil
 }
-
-
