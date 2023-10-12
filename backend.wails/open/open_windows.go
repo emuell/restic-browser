ee5cfabf1d87620d91c1afe9f1001c81e381f467
Eduard Müller <mail@emuell.net>
2023-10-12 22:42:10 +0200

    remove wails backend

--- backend.wails/open/open_windows.go
+++
@@ -1,7 +0,0 @@
-package open
-
-import "golang.org/x/sys/windows"
-
-func openFileOrURL(url string) error {
-	return windows.ShellExecute(0, nil, windows.StringToUTF16Ptr(url), nil, nil, windows.SW_SHOWNORMAL)
-}
